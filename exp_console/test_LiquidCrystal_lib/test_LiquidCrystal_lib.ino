586bed93ba2f84bb355e9ce811072ff111bcc3ae
Bruce Frederiksen <dangyogi@gmail.com>
2022-06-12 11:54:50 -0400

    Paper signage done.

--- exp_console/test_LiquidCrystal_lib/test_LiquidCrystal_lib.ino
+++ exp_console/test_LiquidCrystal_lib/test_LiquidCrystal_lib.ino
@@ -6,46 +6,94 @@ static const uint8_t RESET = 13;  // Built-in LED
 static const uint8_t RS = 5;
 static const uint8_t RW = 11;
 static const uint8_t ENABLE = 12;
-static const uint8_t DB0 = A3;
-static const uint8_t RB0 = 6;
-static const uint8_t DB1 = A2;
-static const uint8_t RB1 = 7;
-static const uint8_t DB2 = A1;
-static const uint8_t RB2 = 8;
-static const uint8_t DB3 = A0;
-static const uint8_t RB3 = 9;
-static const uint8_t DB4 = A6;
-static const uint8_t RB4 = 3;
-static const uint8_t DB5 = A7;
-static const uint8_t RB5 = 2;
+static const uint8_t DB0 = 2;
+//static const uint8_t RB0 = 6;
+static const uint8_t DB1 = 3;
+//static const uint8_t RB1 = 7;
+static const uint8_t DB2 = 6;
+//static const uint8_t RB2 = 8;
+static const uint8_t DB3 = 7;
+//static const uint8_t RB3 = 9;
+static const uint8_t DB4 = 8;
+//static const uint8_t RB4 = 3;
+static const uint8_t DB5 = 9;
+//static const uint8_t RB5 = 2;
 static const uint8_t DB6 = 4;
-static const uint8_t RB6 = 0;   // RX
-static const uint8_t DB7 = 1;   // TX, old 23 (AREF)
-static const uint8_t RB7 = 10;
+//static const uint8_t RB6 = 0;   // RX
+static const uint8_t DB7 = 10;
+//static const uint8_t RB7 = 10;
 
-LiquidCrystal lcd(RS, RW, ENABLE, DB0, DB1, DB2, DB3, DB4, DB5, DB6, DB7);
+//LiquidCrystal lcd(RS, RW, ENABLE, DB0, DB1, DB2, DB3, DB4, DB5, DB6, DB7);
+//LiquidCrystal lcd(RS, ENABLE, DB0, DB1, DB2, DB3, DB4, DB5, DB6, DB7);
+LiquidCrystal lcd(RS, ENABLE, DB4, DB5, DB6, DB7);  // Tie RW to GND
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(RESET, OUTPUT);
-  digitalWrite(RESET, HIGH);
 
-  // Doesn't work with or without these lines:
-  delay(100);
-  digitalWrite(RESET, LOW);
-  delay(100);
-  digitalWrite(RESET, HIGH);
-  delay(100);
-  
-  lcd.begin(24, 1);
-  lcd.print("Hello world!");
+  lcd.begin(16, 2);
+  //lcd.noAutoScroll();
+  //lcd.noCursor();
+  //lcd.setCursor(col, row);   sets location where subsequent text written to the LCD will be displayed
+  lcd.print("Hello World!");
 
   Serial.begin(9600);
-  Serial.setTimeout(5000);    // mSec
+  Serial.setTimeout(500);    // mSec
   Serial.println("setup done");
+  help();
+}
+
+void help(void) {
+  Serial.println();
+  Serial.println("H - help");
+  Serial.println("C - clear");
+  Serial.println("P<col>,<row> - position to (setCursor)");
+  Serial.println("W<text> - write");
+  Serial.println();
+}
+
+byte swallow(byte c) {
+  // Returns 1 on success.
+  byte b = Serial.read();
+  if (b == c) return 1;
+  Serial.print("Expected '");
+  Serial.print(c);
+  Serial.print("', got '");
+  Serial.print(b);
+  Serial.println("'");
+  return 0;
+}
+
+void position(void) {
+  byte col = Serial.parseInt(SKIP_WHITESPACE);
+  if (!swallow(',')) return;
+  byte row = Serial.parseInt(SKIP_WHITESPACE);
+  lcd.setCursor(col, row);
+  Serial.print("Did setCursor to ");
+  Serial.print(col);
+  Serial.print(", ");
+  Serial.println(row);
+}
+
+void write(void) {
+  String text = Serial.readStringUntil('\n');
+  Serial.print("Writing ");
+  Serial.print(text.length());
+  Serial.print(" chars: ");
+  Serial.println(text.c_str());
+  lcd.print(text.c_str());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (Serial.available()) {
+    byte c = Serial.read();
+    switch (toupper(c)) {
+    case 'H': help(); break;
+    case 'C': lcd.clear(); Serial.println("LCD cleared"); break;
+    case 'P': position(); break;
+    case 'W': write(); break;
+    case ' ': case '\t': case '\r': case '\n': break;
+    default: help(); break;
+    } // end switch
+  } // end if (Serial.available())
 }
